c4cd297e8dd42cc64f7bd439821773a7f43a462a
HardCoreWare <bernardocodu@gmail.com>
2019-03-27 15:05:48 -0600

    ok

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'libs/BigQuery.php' ;
 
 $bigQuery= new BigQuery('informe-211921');
 
-$dml = "SELECT DISTINCT(HKONT) AS CUENTAS FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303') AND SUBSTR(HKONT,1,1) = '6'";
+$dml = "SELECT DISTINCT(HKONT) AS CUENTAS FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303','5020100303') AND SUBSTR(HKONT,1,1) = '6'";
 
 $cuentas = $bigQuery->select($dml);
 
@@ -16,7 +16,7 @@ foreach ($cuentas as $cuenta) {
 
     //echo($cuenta['CUENTAS']);
 
-    $dml1="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO, '".$cuenta['CUENTAS']."' AS CUENTA FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303') AND HKONT = '".$cuenta['CUENTAS']."' ";
+    $dml1="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO, '".$cuenta['CUENTAS']."' AS CUENTA FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303','5020100303') AND HKONT = '".$cuenta['CUENTAS']."' ";
 
     $dmlArray[]=$dml1;
 }
