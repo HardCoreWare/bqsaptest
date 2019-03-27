6bc8da7bd86db5222c5613fb451c3ca7ca94af0d
HardCoreWare <bernardocodu@gmail.com>
2019-03-27 14:36:55 -0600

    ok

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $dmlArray=[];
 
 foreach ($cuentas as $cuenta) {
 
-    $dml1="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO, '".$cuenta."' AS CUENTA FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303') AND HKONT ='".$cuenta."' ";
+    $dml1="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO, '".strval($cuenta)."' AS CUENTA FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303') AND HKONT = '".strval($cuenta)."' ";
 
     $dmlArray[]=$dml;
 }
