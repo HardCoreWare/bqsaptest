79d45cee8edec4d7c241e693c7af8d60d559a42b
HardCoreWare <bernardocodu@gmail.com>
2019-03-27 14:12:33 -0600

    cuentas

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 
 require_once 'libs/BigQuery.php' ;
 
+$bigQuery= new BigQuery('informe-211921');
 
-$bigQuery= new BigQuery();
+$dml = "SELECT DISTINCT(HKONT) AS CUENTAS FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303') AND SUBSTR(HKONT,1,1) = '6'";
 
+$cuentas = $bigQuery->select($dml);
 
 ?>
