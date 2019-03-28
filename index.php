d907c46284375cb06a59df6b9fe9fd965818b023
HardCoreWare <bernardocodu@gmail.com>
2019-03-28 01:12:50 -0600

    ok

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="main.js"></script>
-</head>
-<body>
-    <div class="container">
-    
-    
-        <div class="row">
-        
-        
-            <div class="col">
+
             
 <?php
 
@@ -23,52 +6,48 @@ require_once 'libs/BigQuery.php' ;
 
 $bigQuery= new BigQuery('informe-211921');
 
-$dml = "SELECT DISTINCT(HKONT) AS CUENTAS FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303','5020100303') AND SUBSTR(HKONT,1,1) = '6'";
+function testTable($month){
 
-$cuentas = $bigQuery->select($dml);
+    $dml = "SELECT DISTINCT(HKONT) AS CUENTAS FROM `informe-211921.BALANZA.BSEG_2019_'.$month.'` WHERE KOSTL IN('1020100303','5020100303') AND SUBSTR(HKONT,1,1) = '6'";
 
-//print_r($cuentas);
+    $cuentas = $bigQuery->select($dml);
 
-$dmlArray=[];
+    //print_r($cuentas);
 
-foreach ($cuentas as $cuenta) {
+    $dmlArray=[];
 
-    //echo($cuenta['CUENTAS']);
+    foreach ($cuentas as $cuenta) {
 
-    $dml1="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO, '".$cuenta['CUENTAS']."' AS CUENTA FROM `informe-211921.BALANZA.BSEG_2019_2` WHERE KOSTL IN('1020100303','5020100303') AND HKONT = '".$cuenta['CUENTAS']."' ";
+        //echo($cuenta['CUENTAS']);
 
-    $dmlArray[]=$dml1;
+        $dml1="SELECT ROUND(SUM(CAST(DMBTR AS FLOAT64)), 2) AS MONTO, '".$cuenta['CUENTAS']."' AS CUENTA FROM `informe-211921.BALANZA.BSEG_2019_'$month'` WHERE KOSTL IN('1020100303','5020100303') AND HKONT = '".$cuenta['CUENTAS']."' ";
+
+        $dmlArray[]=$dml1;
+    }
+
+    $uniquery = implode(" UNION ALL ", $dmlArray);
+
+    $tablaCuentas =$bigQuery->select($uniquery);
+
+    echo('<table>');
+    echo('<tr><th>CUENTA</th><th>MONTO</th></tr>');
+    foreach ($tablaCuentas as $fila) {
+
+        echo('<tr>');
+        echo('<td>'.$fila['CUENTA'].'</td>');
+        echo('<td>'.$fila['MONTO'].'</td>');
+        echo('</tr>');
+
+
+    }
+
+    echo('</table>');
+
 }
 
-$uniquery = implode(" UNION ALL ", $dmlArray);
-
-$tablaCuentas =$bigQuery->select($uniquery);
-
-echo('<table>');
-echo('<tr><th>CUENTA</th><th>MONTO</th></tr>');
-foreach ($tablaCuentas as $fila) {
-
-    echo('<tr>');
-    echo('<td>'.$fila['CUENTA'].'</td>');
-    echo('<td>'.$fila['MONTO'].'</td>');
-    echo('</tr>');
-
-
-}
-
-echo('</table>');
+testTable('1');
 
 ?>
-
-            
-            </div>
-        
-        
-        </div>
-    
-    
-    </div>
-
 
 
 
