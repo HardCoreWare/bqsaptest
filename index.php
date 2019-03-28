40b460129e144562dacd2872bea1d366d817800e
HardCoreWare <bernardocodu@gmail.com>
2019-03-28 14:52:55 -0600

    corrigiendo conceptos

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ function testTable($title,$month,$concepts){
 
     echo('        
     
-    <div class="col-12">
+    <div class="col-4">
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">'.$title.'</h6>
